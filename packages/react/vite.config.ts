bed097c2250c40747f4cf6839a998682ecc950b5
madonoharu <haruki.kuramoto@labbase.co.jp>
2025-12-17 16:10:25 +0900

    fix

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,6 @@ export default defineConfig({
     }),
   ],
   build: {
-    outDir: "../../docs/react",
+    outDir: path.resolve("../../docs/react"),
   },
 });
--- packages/svelte/vite.config.ts
+++ packages/svelte/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
   build: {
-    outDir: "../../docs/svelte",
+    outDir: path.resolve("../../docs/svelte"),
   },
 });
